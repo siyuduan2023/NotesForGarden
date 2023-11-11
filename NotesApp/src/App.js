bd70304f90ccf8f421be9650d0cf4ad4f1f1d635
Siyu Duan <siyusiyuduan@hotmail.com>
2023-11-11 11:23:03 +0000

    update

---
+++ NotesApp/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Note from './components/Note';
+
+function App() {
+    return (
+        <>
+        <Header />
+        <Note />
+        <Footer />
+        </>
+    )
+}
+export default App;
---
+++ NotesApp/src/components/Header.js
@@ -0,0 +1,8 @@
+function Header() {
+    return (
+        <header>
+        <h1>Garden Notes</h1>
+        </header>
+    )
+}
+export default Header;

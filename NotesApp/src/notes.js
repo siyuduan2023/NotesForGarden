93bb8b1ad64bc4a1d0bde236801b72084365d749
Siyu Duan <siyusiyuduan@hotmail.com>
2023-12-01 17:44:27 +0000

    add month

--- NotesApp/src/notes.js
+++ NotesApp/src/notes.js
@@ -6,7 +6,8 @@ const notes = [
         "Evergreen. Growth 2m * 2m. Take hardwood cuttings throughout summer/autumn. Aromatic pereninal.",
       genus: "Rosemarinus",
       species: "officinalis",
-      family: "Lamiaceae"
+      family: "Lamiaceae",
+      month: "Oct"
     },
     {
       key: 2,
@@ -15,7 +16,8 @@ const notes = [
         "Autumn fruiting Delicious! Can grow 8m by 8m depending on pruning/training. Full sun. Deciduous Tree/hedging plant",
       genus: "Pyrus",
       species: "communis",
-      family: "Rosaceae"
+      family: "Rosaceae",
+      month: "Oct"
     },
     {
       key: 3,
@@ -24,7 +26,8 @@ const notes = [
         "Grows very tall! Seeks the light. Grown from tubers. From 'girosole' - Italian for sunflower Autumn flowering and harvest/division Up to 3/4m tall. Will spread via rhizomes Herbaceous/tuberou s perennial.",
       genus: "Helianthus",
       species: "tuberosus",
-      family: "Asteraceae"
+      family: "Asteraceae",
+      month: "Oct"
     },
     {
       key: 4,
@@ -33,7 +36,8 @@ const notes = [
         "Can be up to a metre and a half Known as a 'broadleaf' weed of lawns and hedges, but is actually very useful. Perennial. Thick Taproot - often useful for drawing minerals up from the soil, excellent to compost.",
       genus: "Rumex",
       species: "obtusifolius",
-      family: "Polyganaceae"
+      family: "Polyganaceae",
+      month: "Oct"
     }
   ];
 
